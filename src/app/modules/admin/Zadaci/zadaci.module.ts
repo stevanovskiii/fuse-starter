b4ecd2ed6a555fbc251a7df80fd582e994fae3a4
AntonioRistevskiINKI533 <antonio.ristevski@uklo.edu.mk>
2021-08-21 22:16:53 +0200

    Tabeli

--- src/app/modules/admin/Zadaci/zadaci.module.ts
+++ src/app/modules/admin/Zadaci/zadaci.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ZadaciComponent } from 'app/modules/admin/zadaci/zadaci.component'
 import { Route, RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { DataTableComponent } from './data-table/data-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 
 
@@ -15,11 +19,15 @@ const zadaciRoutes: Route[] = [
 
 @NgModule({
   declarations: [
-      ZadaciComponent
+      ZadaciComponent,
+      DataTableComponent
   ],
   imports     : [
       RouterModule.forChild(zadaciRoutes),
-      MatIconModule
+      MatIconModule,
+      MatTableModule,
+      MatPaginatorModule,
+      MatSortModule
   ]
 })
 export class ZadaciModule { }
