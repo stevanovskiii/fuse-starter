11832fdd9780f0acb44fc64703ed327a30529a35
stevanovskiii <rstevanovskii@gmail.com>
2021-08-22 16:38:44 +0200

    namesteni fiksni kopcinata no ne izgleda bas naj naj

---
+++ src/app/modules/admin/Zadaci/data-table/data-table.js
@@ -0,0 +1,9 @@
+var elementPosition = $('#button').offset();
+
+$(window).scroll(function(){
+        if($(window).scrollTop() > elementPosition.top){
+              $('#button').css('position','fixed').css('top','0');
+        } else {
+            $('#button').css('position','static');
+        }    
+});
