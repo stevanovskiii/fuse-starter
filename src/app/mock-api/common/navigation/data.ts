af89000d6c9a6fd662aca11c61be3e26b2e3a9d1
AntonioRistevskiINKI533 <antonio.ristevski@uklo.edu.mk>
2021-08-26 23:29:29 +0200

    levo meni sto se otvora podsreden malce

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -17,6 +17,27 @@ export const defaultNavigation: FuseNavigationItem[] = [
         type : 'basic',
         icon : 'heroicons_outline:home',
         link : '/home'
+    },
+    {
+        id   : 'projects',
+        title: 'Проекти',
+        type : 'basic',
+        icon : 'heroicons_outline:clipboard-check',
+        link : '/zadaci'
+    },
+    {
+        id   : 'settings',
+        title: 'Подесувања',
+        type : 'basic',
+        icon : 'heroicons_outline:cog',
+        link : '/settings'
+    },
+    {
+        id   : 'hdome',
+        title: 'Извештаи',
+        type : 'basic',
+        icon : 'heroicons_outline:document-report',
+        link : '/izvestai'
     }
 ];
 
