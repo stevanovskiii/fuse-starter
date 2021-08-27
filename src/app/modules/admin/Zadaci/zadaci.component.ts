7aeb85cc8fec6afcb43fd3b0de13d7ed05ff714f
Rade Stevanovski <rade.stevanovski@uklo.edu.mk>
2021-08-27 19:21:14 +0200

    Sredeni filtrite za prebaruvanje

--- src/app/modules/admin/Zadaci/zadaci.component.ts
+++ src/app/modules/admin/Zadaci/zadaci.component.ts
@@ -1,6 +1,8 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { CreateTaskComponent } from 'app/layout/common/create-task/create-task.component';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 
 @Component({
@@ -28,4 +30,20 @@ export class ZadaciComponent
         dialogConfig.minWidth = "295px";
         this.dialog.open(CreateTaskComponent,dialogConfig)
     }
+    OtvoriFiltriBool : boolean = false;
+    OtvoriFiltri(){
+        if(this.OtvoriFiltriBool==false){ 
+          let a = window.document.getElementById('SkrieniKopcina')!;
+          a.style.display='block';
+          this.OtvoriFiltriBool = true;
+        }
+        else if (this.OtvoriFiltriBool==true){ 
+          let a = window.document.getElementById('SkrieniKopcina')!;
+          a.style.display='none';
+          this.OtvoriFiltriBool = false;
+        }
+    }
+    selectedProject: string = 'ACME Corp. Backend App';
+    private _unsubscribeAll: Subject<any> = new Subject<any>();
+    
 }
